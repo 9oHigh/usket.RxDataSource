6f4c127221f32e5e5b598f6f29d191814bc4030c
9oHigh <usket@icloud.com>
2023-09-22 14:18:30 +0900

    feat: 프로젝트 추가

---
+++ usket.RxDataSource/usket.RxDataSource/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  usket.RxDataSource
+//
+//  Created by 이경후 on 2023/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
---
+++ usket.RxDataSource/usket.RxDataSourceUITests/usket_RxDataSourceUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  usket_RxDataSourceUITestsLaunchTests.swift
+//  usket.RxDataSourceUITests
+//
+//  Created by 이경후 on 2023/09/22.
+//
+
+import XCTest
+
+final class usket_RxDataSourceUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
